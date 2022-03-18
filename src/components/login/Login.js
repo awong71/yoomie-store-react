1aa1440b3fb6812992d605c127964e34593aad69
awong71 <albertjameswong@ymail.com>
2022-03-17 18:25:22 -0600

    fsdgr

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,27 +1,53 @@
-import React from 'react';
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate} from 'react-router-dom'
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './../../libs/firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { IoToggleSharp } from 'react-icons/io5';
 import {LoginContainer, LoginForms, LoginForm, LoginLogoImage, LoginButton, LoginToggle, LoginText} from './styles'
 
 function Login(props) {
     const navigation = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const notify = (error) => toast.error(error.code,{
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+    });
+
     function onSubmitHandler(e){
         e.preventDefault();
-        navigation('/dashboard')
+        signInWithEmailAndPassword(auth, email, password)
+        .then(userCredential=>{
+            navigation('/dashboard')
+        })
+        .catch(error=>{
+            notify(error)
+        })
     }
 
     return ( 
         <LoginContainer style={{ backgroundImage: "url(/img/login-bg.jpg)"}}>
-            <LoginLogoImage src="/img/logo-wName.png" alt="Yoomie logo"></LoginLogoImage>
-            
+            <LoginLogoImage src="/img/logo-nobg.png" alt="Yoomie logo"></LoginLogoImage>
 
+        <ToastContainer/>
 
         <LoginForms onSubmit={onSubmitHandler}>
-            <LoginForm required className="userNameForm" type="text" id="userName" name="userName" placeholder="Username"></LoginForm>
+            <LoginForm required className="userNameForm" type="text" id="userName" name="userName" placeholder="Username" 
+            onChange={(e)=>setEmail(e.target.value)}></LoginForm>
         
         
-            <LoginForm required className="passwordForm" type="password" id="password" name="password" placeholder="Password"></LoginForm>
+            <LoginForm required className="passwordForm" type="password" id="password" name="password" placeholder="Password"
+            onChange={(e)=>setPassword(e.target.value)}></LoginForm>
 
             <LoginButton className="loginButton" type="submit">GET STARTED</LoginButton>
 
